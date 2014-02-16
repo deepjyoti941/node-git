c552636409ba2c22f8f3ab92aadf19ef3c6b09d9
deepjyoti941 <deepjyoti941@gmail.com>
2014-02-17 01:38:25 +0530

    requesting github api

---
+++ request.js
@@ -0,0 +1,24 @@
+var https = require("https");
+
+var username = 'OllieParsley';
+
+var options = {
+	host: 'api.github.com',
+	path: '/users/'+username+'/repos',
+	method: 'GET'
+};
+
+var request = https.request(options, function(response){
+	
+	var body = '';
+	
+	response.on("data" , function(chunk){
+		body += chunk.toString('utf8');
+	});
+	response.on("end" , function(){
+		console.log("Body:" , body);
+	});
+
+});
+
+request.end();
