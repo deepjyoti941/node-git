71f65cdf458524dcd7d6dd3d9a8ea42371a9a566
deepjyoti941 <deepjyoti941@gmail.com>
2014-02-17 23:57:45 +0530

    crconnecting to mongodb

---
+++ connect.js
@@ -0,0 +1,9 @@
+var mongo = require("mongodb");
+var host = "127.0.0.1";
+var port = mongo.Connection.DEFAULT_PORT;
+var db = new mongo.Db("nodejs-introduction" , new mongo.Server(host , port , {})); //creating database with name nodejs-introduction
+
+db.open(function(error){
+	console.log("We are connected " +host+":"+port);
+
+});
