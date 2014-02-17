6bb9deabd5889b81f51363e2bb30fddbf698a88d
deepjyoti941 <deepjyoti941@gmail.com>
2014-02-17 23:31:53 +0530

    creating module for github api

---
+++ github.js
@@ -0,0 +1,42 @@
+var https = require("https");
+
+function getRepos(username, callback) {
+
+	var options = {
+		headers: {'user-agent': 'deepjyoti941'},
+		host: 'api.github.com',
+		path: '/users/'+username+'/repos',
+		method: 'GET'
+	};
+
+	var request = https.request(options, function(response){
+		
+		var body = '';
+		
+		response.on("data" , function(chunk){
+			body += chunk.toString('utf8');
+		});
+		response.on("end" , function(){
+			var repos = [];
+			var json = JSON.parse(body);
+			json.forEach(function(repo){
+				repos.push({
+					name: repo.name,
+					description: repo.description
+				});
+			})
+			//console.log("Repos:" ,repos);
+			//console.log("Count:" , json.length);
+			callback(repos);
+		});
+
+	});
+
+	request.end();
+
+}
+
+module.exports.getRepos = getRepos;
+
+
+
--- test_module.js
+++ test_module.js
@@ -1,2 +1,7 @@
 var myModule = require("./my_module.js");
+var github = require("./github.js");
 console.log("Hello: " , myModule.hello());
+
+github.getRepos("deepjyoti941" , function(repos){
+	console.log("Deepjyoti repos ", repos);
+})
