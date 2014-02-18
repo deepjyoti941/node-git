1f3881c7a8308b6e777c00fe838c98dd134e94fa
deepjyoti941 <deepjyoti941@gmail.com>
2014-02-18 14:18:55 +0530

    wrapping it in a function

--- query.js
+++ query.js
@@ -1,23 +1,35 @@
 var mongo = require("mongodb");
 var host = "127.0.0.1";
 var port = mongo.Connection.DEFAULT_PORT;
-var db = new mongo.Db("nodejs-introduction" , new mongo.Server(host , port , {})); //creating database with name nodejs-introduction
 
-db.open(function(error){
-	console.log("We are connected " +host+":"+port);
+//wrapping it inside a function
+function getUser(id,callback) {
 
-	//creating user collection i.e user table
-	db.collection("user" , function(error , collection){ 
-		console.log("we have the collection");
+	var db = new mongo.Db("nodejs-introduction" , new mongo.Server(host , port , {})); //creating database with name nodejs-introduction
+	db.open(function(error){
+		console.log("We are connected " +host+":"+port);
 
-		collection.find({"id":"1"} , function(error , cursor){
-			cursor.toArray(function(error , users){
-				if(users.length == 0) {
-					console.log("user not found");
-				} else {
-					console.log("Found a user" , users[0]);
-				}
+		//creating user collection i.e user table
+		db.collection("user" , function(error , collection){ 
+			console.log("we have the collection");
+
+			collection.find({"id":id.toString()} , function(error , cursor) {
+				cursor.toArray(function(error , users){
+					if(users.length == 0) {
+						callback(false);
+					} else {
+						callback(users[0]);
+					}
+				});
 			});
 		});
 	});
+}
+
+getUser(1,function(user) {
+	if (!user) {
+		console.log('No user found');
+	} else {
+		console.log('we have a user: ' , user);
+	}
 });
